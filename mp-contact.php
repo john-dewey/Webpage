abd6e27c72e91136465e6e7ed24a761d926f2246
Johnathan Dewey <jtdewey2@gmail.com>
2024-07-06 19:06:24 -0400

    Contact Completed

--- mp-contact.php
+++ mp-contact.php
@@ -4,7 +4,7 @@
 <head>
     <title>Contact</title>
     <link rel="icon" type="image/x-icon" href="resources\media\logo-jtd-fauxicon.jpg">
-    <link rel="stylesheet" href="css\p-contact.css">
+    <link rel="stylesheet" href="css\p-contatct.css">
     <link rel="stylesheet" href="css\f-graphics.css">
     <link rel="stylesheet" href="css\s-override.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,79 +15,83 @@
 
 <body>
     <header>
-            <?php
-            include 'f-header.php';
-            ?>
+        <?php
+        include 'f-header.php';
+        ?>
     </header>
 
-    <!-- Main -->
-    <div class="main">
-        <div class="main-header">
+    <!-- Top Level -->
+    <div class="row">
+        <div class="col-12 main-header">
             <h1>Find Me...</h1>
         </div>
-        <div class="main-body">
+    </div>
 
-            <!-- Left -->
-            <div class="body-left">
+    <!-- Second Level -->
+    <div class="row">
+        <!-- Work -->
+        <div class="col-6 main-container">
 
-                <div class="body-header">
-                    <h2>At Work</h2>
+            <!-- Header -->
+            <div class="container-header">
+                <h2>At Work</h2>
+            </div>
+
+            <!-- Work Information -->
+            <div class="container-box">
+
+                <button class="work-bttn" onclick="work()">
+                    <h3>Student at Furman University<br></h3>
+                    <h4>&ensp;&ensp;3300 Poinsett Hwy PMB 27852 <br>&ensp;&ensp;Greenville, SC 29613 USA</h4>
+                </button>
+
+                <div class="map">
+                    <?php
+                    include 'f-map.php';
+                    ?>
                 </div>
+            </div>
+        </div>
 
-                <div class="main-container">
+        <!-- Socials -->
+        <div class="col-6 main-container">
 
-                    <button class="work-bttn" onclick="work()">
-                            <h3>Student at Furman University<br></h3>
-                            <h4>&ensp;&ensp;3300 Poinsett Hwy PMB 27852 <br>&ensp;&ensp;Greenville, SC 29613 USA</h4>
+            <!-- Header -->
+            <div class="container-header">
+                <h2>Or Online</h2>
+            </div>
+
+            <!-- Social Information -->
+            <div class="container-box">
+                <div class="grid">
+                    <button class="social-bttn" onclick="social1()">
+                        <div class="graphic-X"></div>
                     </button>
 
-                    <div class="map">
-                        <?php
-                            include 'f-map.php';
-                        ?>
-                    </div>
+                    <button class="social-bttn" onclick="social2()">
+                        <div class="graphic-mail"></div>
+                    </button>
 
+                    <button class="social-bttn" onclick="social3()">
+                        <div class="graphic-linkedin"></div>
+                    </button>
+
+                    <button class="social-bttn" onclick="social4()">
+                        <div class="graphic-github"></div>
+                    </button>
                 </div>
-
             </div>
-
-            <!-- Right -->
-            <div class="body-right">
-
-                <div class="body-header">
-                    <h2>Or Online</h2>
-                </div>
-                <div class="main-container">
-                    <div class="grid">
-                        <button class="social-bttn" onclick="social1()">
-							<div class="graphic-X"></div>
-						</button>
-
-                        <button class="social-bttn" onclick="social2()">
-							<div class="graphic-mail"></div>
-						</button>
-
-                        <button class="social-bttn" onclick="social3()">
-							<div class="graphic-linkedin"></div>
-						</button>
-
-                        <button class="social-bttn" onclick="social4()">
-							<div class="graphic-github"></div>
-						</button>
-                    </div>
-                </div>
-
-            </div>
-
         </div>
+
 
     </div>
 
+
     <footer>
-	<?php
-		include 'f-footer.php';
-		?>
-	</footer>
+        <?php
+        include 'f-footer.php';
+        ?>
+    </footer>
 
 </body>
 
